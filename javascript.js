4ac0a70b6e95dc6d378f876bca8a641039935348
NasserGh93 <nasserghaibeh@googlemail.com>
2026-03-29 08:48:15 +0200

    notes

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// save global
+// i need notes
+
+let notes = ['banana', 'Orange'];
+
+function renderNotes() {
+    let contentRef = document.getElementById('content')
+    contentRef.innerHTML = notes;
+}
+// where are the notes to see
+//->when the notes
+//add notes
+// delete notes
+//archive notes
